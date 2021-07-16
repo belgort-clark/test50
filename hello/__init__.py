a2d4744eb38e1c18d278ac646a8f4b65a1fd24b2
Bruce Elgort <belgort>
2021-07-16 08:41:56 -0700

    mods

--- hello/__init__.py
+++ hello/__init__.py
@@ -11,7 +11,7 @@ def prints_hello():
     """prints "hello, world\\n" """
     check50.run("python3 hello.py").stdout("[Hh]ello, [Ww]orld!?\n", regex=True).exit(0)
 
-@check50.check()
+@check50.check(exists)
 def id():
     """hello.py prints what you give it"""
     check50.run("python3 hello.py").stdin("foo").stdout("foo").stdin("bar").stdout("bar")
