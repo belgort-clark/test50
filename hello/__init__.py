11e4b0cb688991b398e7ea7f49ba92df3cbf22e3
Bruce Elgort <belgort>
2021-07-16 08:42:55 -0700

    mods

--- hello/__init__.py
+++ hello/__init__.py
@@ -7,9 +7,9 @@ def exists():
     check50.exists("hello.py")
 
 @check50.check(exists) # only run this check if the exists check has passed
-def prints_hello():
-    """prints "hello, world\\n" """
-    check50.run("python3 hello.py").stdout("[Hh]ello, [Ww]orld!?\n", regex=True).exit(0)
+#def prints_hello():
+#    """prints "hello, world\\n" """
+#    check50.run("python3 hello.py").stdout("[Hh]ello, [Ww]orld!?\n", regex=True).exit(0)
 
 @check50.check(exists)
 def id():
